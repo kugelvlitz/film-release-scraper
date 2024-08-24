b72ce8c547d3084dd683f90d4d3a7d81e806ce4c
David Azoherra <david.azoherra@protonmail.com>
2024-08-24 01:46:03 -0600

    Minor improvements

--- novacinemas_scraper.py
+++ novacinemas_scraper.py
@@ -2,33 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+class NovacinemasScraper:
+    def __init__(self):
+        self.soup = BeautifulSoup(requests.get("https://www.novacinemas.cr/").content, "html.parser")
 
-def get_titles(release: str) -> list:
-    soup = BeautifulSoup(requests.get("https://www.novacinemas.cr/").content, "html.parser")
+    def get_titles(self, release: str) -> list:
 
-    release_class_label = ""
-    if release == "upcoming":
-        release_class_label = "upcoming"
-    elif release == "current":
-        release_class_label = "blockPremiere"
-    else:
-        print("Invalid type of release")
-        return None
+        # Depending on the value of release look for films now playing or upcoming releases
+        release_class_label = ""
+        if release == "upcoming":
+            release_class_label = "upcoming"
+        elif release == "current":
+            release_class_label = "blockPremiere"
+        else:
+            print("Invalid type of release")
+            return None
 
-    film_divs = soup.find_all("section", class_=release_class_label)
+        films = self.soup.find_all("section", class_=release_class_label)[0]
+        itemlist_div = films.find_all("div", class_="itemsList")[0]
 
-    # La página contiene una única sección con la clase upcoming, donde están los próximos estrenos
-    films = film_divs[0]
+        title_list = []
+        for item in itemlist_div:
+            title = item.find_all('h3')[0].text
+            if title != "":
+                title_list += [title]
 
-    # upcoming contienen una única item list
-    itemlist_div = films.find_all("div", class_="itemsList")[0]
+        return title_list
 
-    title_list = []
-    for item in itemlist_div:
-        title = item.find_all('h3')[0].text
-        if title != "":
-            title_list += [title]
+    def get_latest_showing():
+        pass
 
-    return title_list
+    def get_showing_times():
+        pass
 
-print(get_titles("current"))
+print(NovacinemasScraper().get_titles("upcoming"))
